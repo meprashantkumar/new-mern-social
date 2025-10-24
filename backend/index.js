30fae31af184d09a727e032bc8d0349987520f3e
Prashant <prashantchoubey8338@gmail.com>
2025-10-24 11:25:11 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -8,30 +8,6 @@ import { isAuth } from "./middlewares/isAuth.js";
 import { User } from "./models/userModel.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
-import axios from 'axios';
-
-const url = `https://mern-social-3e3m.onrender.com`;
-const interval = 30000;
-
-function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(
-        `Reloaded at ${new Date().toISOString()}: Status Code ${
-          response.status
-        }`
-      );
-    })
-    .catch((error) => {
-      console.error(
-        `Error reloading at ${new Date().toISOString()}:`,
-        error.message
-      );
-    });
-}
-
-setInterval(reloadWebsite, interval);
 
 dotenv.config();
 
